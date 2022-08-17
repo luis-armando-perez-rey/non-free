53285149d5ceecc8f57777c0c4c10fd742a57e22
giovanni-marchetti <glma@kth.se>
2022-08-17 10:29:54 +0200

    minor

--- run.sh
+++ run.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-python3 main.py --model-name 'TMP' --model 'cnn' --dataset 'rot-square'  --num 5 --extra-dim 2 --batch-size 128   --epochs 100
-  
+python3 main.py --model-name 'TMP' --model 'cnn' --dataset 'rot-square'  --num 5 --extra-dim 0 --batch-size 128   --epochs 100
