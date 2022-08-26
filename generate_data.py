25db02321575aa64aa0ae6f3dd4c4cbe89e34486
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2022-08-26 11:11:06 +0200

    Update generate_data.py
    Add flag for multicolor arrows

--- generate_data.py
+++ generate_data.py
@@ -8,6 +8,7 @@ parser.add_argument('--dataset_name', default='4', type=str, help="Dataset")
 parser.add_argument("--n_arrows", nargs="+", dest="n_arrows", type=int, default=[4],
                         help="Number of arrows to generate in dataset")
 parser.add_argument("--n_examples", dest="n_examples", default=1000, type = int, help="Number of examples per num arrows")
+parser.add_argument('--multicolor', default=False, action='store_true')
 args = parser.parse_args()
 
 
@@ -17,6 +18,8 @@ def generate_dataset(dataset):
                                      dataset_folder="./data/arrows",
                                      dataset_name=args.dataset_name
                                      )
+        if args.multicolor:
+            generation_parameters["multicolor"] = True
         generate_training_data(**generation_parameters, examples_per_num_arrows=args.n_examples)
         generate_eval_data(**generation_parameters)
 
