76269e3ff8614d0a3c08758ff09b8253ab5c5c3f
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2022-08-26 11:16:01 +0200

    Update bash files

--- generate_data.sh
+++ generate_data.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
-python3 generate_data.py --n_examples 10000 --dataset_name 34 --n_arrows 3 4
 python3 generate_data.py --n_examples 10000 --dataset_name 3 --n_arrows 3
-python3 generate_data.py --n_examples 10000 --dataset_name 4 --n_arrows 4
+python3 generate_data.py --n_examples 10000 --dataset_name 4_m --n_arrows 4 --multicolor
+
--- run.sh
+++ run.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-
-#python3 main.py --model-name 'TMP' --checkpoints-dir "./saved_models" --model 'cnn' --dataset 'rot-arrows' --dataset_name "4"  --num 5 --extra-dim 0 --batch-size 128 --epochs 100 --data-dir ./data
-python3 main.py --model-name 'TMP3-2' --checkpoints-dir "./saved_models" --model 'cnn' --dataset 'rot-arrows' --dataset_name "3"  --num 4 --extra-dim 0 --batch-size 128 --epochs 100 --data-dir ./data
+identity_loss="infonce"
+dataset_name="1_m"
+N=1
+extra_dim=0
+experiment_id="1"
+epochs=200
+model_name="D$dataset_name-L$identity_loss-ED$extra_dim-N$N-ID$experiment_id"
+python3 main.py --model-name ${model_name} --checkpoints-dir "./saved_models" --model 'cnn' --dataset 'rot-arrows' --identity-loss ${identity_loss} --dataset_name ${dataset_name}  --num ${N} --extra-dim ${extra_dim} --batch-size 128 --epochs ${epochs} --data-dir ./data
+python3 visualize_latent_space.py --save-folder=${model_name} --dataset=rot-arrows --dataset_name ${dataset_name}
