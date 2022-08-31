bb6331cf788a1afeeeb2b4da2db3063c2c587d17
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2022-08-31 09:27:36 +0200

    Update rotating_arrows.py

--- dataset_generation/rotating_arrows.py
+++ dataset_generation/rotating_arrows.py
@@ -5,16 +5,12 @@ import matplotlib.patches as mpatches
 from skimage.transform import resize
 from typing import Tuple, Optional, List
 
-AVAILABLE_TAB_COLORS = ["tab:red", "tab:green", "tab:purple","tab:orange", "tab:blue",   "tab:brown", "tab:pink",
+AVAILABLE_TAB_COLORS = ["tab:red", "tab:green", "tab:purple", "tab:orange", "tab:blue", "tab:brown", "tab:pink",
                         "tab:gray", "tab:olive", "tab:cyan"]
 
 
 class ArrowCanvas:
-    def __init__(self, num_arrows: int, style: str, resolution: Tuple[int] = (64, 64), radius: float = 1.0):
-
-        self.num_arrows = num_arrows
-        self.style = style
-        self.radius = radius
+    def __init__(self, resolution: Tuple[int] = (64, 64)):
         self.resolution = resolution
         self.fig, self.ax = self._set_figax()
 
@@ -31,17 +27,6 @@ class ArrowCanvas:
         return fig, ax
 
     @property
-    def radius(self):
-        return self._radius
-
-    @radius.setter
-    def radius(self, radius):
-        if self.style == "fork":
-            self._radius = radius * 0.8
-        else:
-            self._radius = radius
-
-    @property
     def numpy_img(self):
         self.fig.canvas.draw()
         array = np.frombuffer(self.fig.canvas.tostring_rgb(), dtype=np.uint8)
@@ -56,59 +41,65 @@ class ArrowCanvas:
         """
         return np.transpose(self.numpy_img, (2, 0, 1))
 
-    @property
-    def _mutation_scale(self):
-        if self.num_arrows == 1:
+    @staticmethod
+    def get_mutation_scale(num_arrows):
+        if num_arrows == 1:
             return 200
         else:
             return 100
 
-    @property
-    def _arrow_angles(self):
-        angles = 2 * np.pi * np.linspace(0, 1, self.num_arrows, endpoint=False)
+    @staticmethod
+    def get_arrow_angles(num_arrows):
+        angles = 2 * np.pi * np.linspace(0, 1, num_arrows, endpoint=False)
         return angles
 
-    @property
-    def arrow_params(self):
-        if self.style == "simple":
+    @staticmethod
+    def get_arrow_params(style):
+        if style == "simple":
             params = dict(linewidth=0.0,
                           arrowstyle=mpatches.ArrowStyle("simple"))
-        elif self.style == "fancy":
+        elif style == "fancy":
             params = dict(linewidth=0.0,
                           arrowstyle=mpatches.ArrowStyle("fancy", head_length=0.5, head_width=0.5, tail_width=0.3))
-        elif self.style == "wedge":
+        elif style == "wedge":
             params = dict(linewidth=0.0,
                           arrowstyle=mpatches.ArrowStyle("wedge", head_length=0.5, tail_width=0.3))
-        elif self.style == "fork":
+        elif style == "fork":
             params = dict(linewidth=10.0,
                           arrowstyle=mpatches.ArrowStyle("-[", widthB=0.2, lengthB=0.2))
-        elif self.style == "triangle":
+        elif style == "triangle":
             params = dict(linewidth=0.0,
                           arrowstyle=mpatches.ArrowStyle("-|>", head_length=0.5))
         else:
             raise ValueError("Invalid arrow style, valid options are 'simple', 'fancy', 'wedge', 'fork', 'triangle'")
         return params
 
-    def _add_arrow(self, arrow_angle, color):
-        arrow = mpatches.FancyArrowPatch((0, 0), (self.radius * np.cos(arrow_angle), self.radius * np.sin(arrow_angle)),
+    def _add_arrow(self, arrow_angle, num_arrows, color, start_point=(0, 0), radius=1.0, style="simple"):
+        arrow_params = self.get_arrow_params(style)
+        mutation_scale = self.get_mutation_scale(num_arrows)
+        arrow = mpatches.FancyArrowPatch(start_point, (
+            start_point[0] + radius * np.cos(arrow_angle), start_point[1] + radius * np.sin(arrow_angle)),
                                          edgecolor=color, facecolor=color,
-                                         mutation_scale=self._mutation_scale,
+                                         mutation_scale=mutation_scale,
                                          shrinkA=0,
-                                         **self.arrow_params)
+                                         **arrow_params)
         self.ax.add_patch(arrow)
 
-    def add_arrows(self, rotation_rad, color=AVAILABLE_TAB_COLORS[0]):
-        for arrow_angle in self._arrow_angles:
-            self._add_arrow(arrow_angle + rotation_rad, color)
+    def add_arrows(self, rotation_rad, num_arrows, color=AVAILABLE_TAB_COLORS[0], start_point=(0, 0), radius=1.0,
+                   style="simple"):
+        arrow_angles = self.get_arrow_angles(num_arrows)
+        for arrow_angle in arrow_angles:
+            self._add_arrow(arrow_angle + rotation_rad, num_arrows, color, start_point, radius, style)
 
-    def add_multicolor_arrows(self, rotation_rad, colors=None):
+    def add_multicolor_arrows(self, rotation_rad, num_arrows, colors=None, start_point=(0, 0), radius=1.0,
+                              style="simple"):
+        arrow_angles = self.get_arrow_angles(num_arrows)
         if colors is None:
             colors = AVAILABLE_TAB_COLORS
-        assert len(colors) >= len(
-            self._arrow_angles), f"Total colors {len(colors)} provided is not enough for {len(self._arrow_angles)} " \
-                                 f"arrows "
-        for num_arrow, arrow_angle in enumerate(self._arrow_angles):
-            self._add_arrow(arrow_angle + rotation_rad, colors[num_arrow])
+        assert len(colors) >= len(arrow_angles), f"Total colors {len(colors)} provided is not enough for " \
+                                                 f"{len(arrow_angles)} arrows "
+        for num_arrow, arrow_angle in enumerate(arrow_angles):
+            self._add_arrow(arrow_angle + rotation_rad, num_arrows, colors[num_arrow], start_point, radius, style)
 
     @staticmethod
     def show():
@@ -136,12 +127,11 @@ def generate_training_data(num_arrows_list, dataset_folder, dataset_name, style_
                 for radius in radius_list:
                     for num_example in range(examples_per_num_arrows):
                         print(
-                            "Generating image with num arrows {}, style {}, color {}, radius {}, multicolor {}, num example {}".format(
+                            "Generating image with num arrows {}, style {}, color {}, radius {}, multicolor {}, "
+                            "num example {}".format(
                                 num_arrows, style, color, radius, multicolor, num_example))
-                        c1 = ArrowCanvas(num_arrows=num_arrows, style=style, radius=radius,
-                                         resolution=resolution)
-                        c2 = ArrowCanvas(num_arrows=num_arrows, style=style, radius=radius,
-                                         resolution=resolution)
+                        c1 = ArrowCanvas(resolution=resolution)
+                        c2 = ArrowCanvas(resolution=resolution)
                         angle1 = 2 * np.pi * np.random.random()
                         angle2 = 2 * np.pi * np.random.random()
 
@@ -149,68 +139,15 @@ def generate_training_data(num_arrows_list, dataset_folder, dataset_name, style_
                             assert len(
                                 color_list) == 1, "When plotting multicolor arrows don't provide a color_list of " \
                                                   "length higher than 1 to avoid unnecesary data creation"
-                            c1.add_multicolor_arrows(rotation_rad=angle1)
-                            c2.add_multicolor_arrows(rotation_rad=angle2)
+                            c1.add_multicolor_arrows(rotation_rad=angle1, num_arrows=num_arrows, colors=None,
+                                                     radius=radius, style=style)
+                            c2.add_multicolor_arrows(rotation_rad=angle2, num_arrows=num_arrows, colors=None,
+                                                     radius=radius, style=style)
                         else:
-                            c1.add_arrows(rotation_rad=angle1, color=color)
-                            c2.add_arrows(rotation_rad=angle2, color=color)
-
-                        img1 = c1.numpy_torch_img
-                        img2 = c2.numpy_torch_img
-                        angle = angle2 - angle1
-
-                        equiv_data.append([img1, img2])
-                        equiv_lbls.append(angle)
-                        equiv_stabilizers.append(num_arrows)
-                        plt.close("all")
-    equiv_data = np.array(equiv_data)
-    equiv_lbls = np.array(equiv_lbls)
-    equiv_stabilizers = np.array(equiv_stabilizers)
-
-    np.save(os.path.join(dataset_folder, dataset_name + '_data.npy'), equiv_data)
-    np.save(os.path.join(dataset_folder, dataset_name + '_lbls.npy'), equiv_lbls)
-    np.save(os.path.join(dataset_folder, dataset_name + '_stabilizers.npy'), equiv_stabilizers)
-
-
-def generate_training_data_png(num_arrows_list, dataset_folder, dataset_name, style_list: Optional[List[str]] = None,
-                               color_list: Optional[List[str]] = None, radius_list: Optional[List[float]] = None,
-                               num_angles: int = 36,
-                               resolution=(64, 64), multicolor=False):
-    equiv_data = []
-    equiv_lbls = []
-    equiv_stabilizers = []
-    if not os.path.exists(dataset_folder):
-        os.makedirs(dataset_folder)
-    if style_list is None:
-        style_list = ["simple", "fancy", "wedge", "fork", "triangle"]
-    if color_list is None:
-        color_list = ["tab:blue", "tab:orange", "tab:green", "tab:red", "tab:purple"]
-    if radius_list is None:
-        radius_list = [1.0]
-    for num_arrows in num_arrows_list:
-        for style in style_list:
-            for color in color_list:
-                for radius in radius_list:
-                    for num_example in range(examples_per_num_arrows):
-                        print(
-                            "Generating image with num arrows {}, style {}, color {}, radius {}, multicolor {}, num example {}".format(
-                                num_arrows, style, color, radius, multicolor, num_example))
-                        c1 = ArrowCanvas(num_arrows=num_arrows, style=style, radius=radius,
-                                         resolution=resolution)
-                        c2 = ArrowCanvas(num_arrows=num_arrows, style=style, radius=radius,
-                                         resolution=resolution)
-                        angle1 = np.pi * np.random.random()
-                        angle2 = np.pi * np.random.random()
-
-                        if multicolor:
-                            assert len(
-                                color_list) == 1, "When plotting multicolor arrows don't provide a color_list of " \
-                                                  "length higher than 1 to avoid unnecesary data creation"
-                            c1.add_multicolor_arrows(rotation_rad=angle1)
-                            c2.add_multicolor_arrows(rotation_rad=angle2)
-                        else:
-                            c1.add_arrows(rotation_rad=angle1, color=color)
-                            c2.add_arrows(rotation_rad=angle2, color=color)
+                            c1.add_arrows(rotation_rad=angle1, num_arrows=num_arrows, color=color,
+                                          radius=radius, style=style)
+                            c2.add_arrows(rotation_rad=angle2, num_arrows=num_arrows, color=color,
+                                          radius=radius, style=style)
 
                         img1 = c1.numpy_torch_img
                         img2 = c2.numpy_torch_img
@@ -252,15 +189,16 @@ def generate_eval_data(num_arrows_list, dataset_folder, dataset_name, style_list
                     images_per_object = []
                     for num_angle, angle in enumerate(2 * np.pi * np.linspace(0, 1, total_rotations)):
                         print(num_arrows, num_angle)
-                        c = ArrowCanvas(num_arrows=num_arrows, style=style, radius=radius,
-                                        resolution=resolution)
+                        c = ArrowCanvas(resolution=resolution)
                         if multicolor:
                             assert len(
                                 color_list) == 1, "When plotting multicolor arrows don't provide a color_list of " \
                                                   "length higher than 1 to avoid unnecesary data creation"
-                            c.add_multicolor_arrows(rotation_rad=angle)
+                            c.add_multicolor_arrows(rotation_rad=angle, num_arrows=num_arrows, colors=None,
+                                                    radius=radius, style=style)
                         else:
-                            c.add_arrows(rotation_rad=angle, color=color)
+                            c.add_arrows(rotation_rad=angle, num_arrows=num_arrows, color=color,
+                                         radius=radius, style=style)
                         img = c.numpy_torch_img
                         images_per_object.append(img)
                         plt.close("all")
@@ -270,3 +208,14 @@ def generate_eval_data(num_arrows_list, dataset_folder, dataset_name, style_list
     stabilizers = np.array(stabilizers)
     np.save(os.path.join(dataset_folder, dataset_name + '_eval_data.npy'), images)
     np.save(os.path.join(dataset_folder, dataset_name + '_eval_stabilizers.npy'), stabilizers)
+
+
+def generate_two_arrow(num_arrows_pairs, dataset_folder, dataset_name, style_combinations: Optional[List[str]] = None,
+                       color_pairs: Optional[List[str]] = None,
+                       total_rotations: int = 36, examples_per_pair: int = 100,
+                       resolution=(64, 64), multicolor=False):
+    num_combinations = len(num_arrows_pairs)
+    c1 = ArrowCanvas(resolution)
+    c1.add_arrows(0, num_arrows=3, color=AVAILABLE_TAB_COLORS[0], start_point=(0.5, 0.5), radius=0.5)
+    c1.add_arrows(0, num_arrows=4, color=AVAILABLE_TAB_COLORS[1], start_point=(-0.5, -0.5), radius=0.5)
+    c1.show()
