4da0347e0f521776738fadc7c06268c57c29eed8
giovanni-marchetti <glma@kth.se>
2022-09-02 09:31:34 +0200

    Chamfer on validation

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/bin/bash
 identity_loss="infonce"
-dataset_name="4 7"
+dataset_name="4 5 6 7"
 dataset="rot-arrows"
 N=7
 extra_dim=0
@@ -10,7 +10,7 @@ optimizer="adamw"
 model="resnet"
 save_interval=1
 lr=1e-4
-batch_size=256
+batch_size=16
 equiv_loss="binary"
 model_name="D${dataset_name// /_}-L$identity_loss-ED$extra_dim-N$N-M$model-A$model-ID$experiment_id"
 python3 main.py --model ${model} --model-name ${model_name} --lr ${lr} --checkpoints-dir "./saved_models" --optimizer ${optimizer} --dataset ${dataset} --identity-loss ${identity_loss} --dataset_name ${dataset_name}  --num ${N} --extra-dim ${extra_dim} --batch-size ${batch_size} --epochs ${epochs} --data-dir ./data --save-interval ${save_interval} --plot 0 --equiv-loss ${equiv_loss}
