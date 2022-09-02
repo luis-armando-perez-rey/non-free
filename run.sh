f5e491d937b1de0e65a2ecc02189533a48343995
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2022-09-02 10:22:32 +0200

    Update run.sh
    Add also the type of dataset to the saved model name

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ dataset_name="4 5 6 7"
 dataset="rot-arrows"
 N=7
 extra_dim=0
-experiment_id="rep3"
+experiment_id=""
 epochs=100
 optimizer="adamw"
 model="resnet"
@@ -12,6 +12,6 @@ save_interval=1
 lr=1e-4
 batch_size=16
 equiv_loss="binary"
-model_name="D${dataset_name// /_}-L$identity_loss-ED$extra_dim-N$N-M$model-A$model-ID$experiment_id"
+model_name="D${dataset}-D${dataset_name// /_}-L$identity_loss-ED$extra_dim-N$N-M$model-A$model-ID$experiment_id"
 python3 main.py --model ${model} --model-name ${model_name} --lr ${lr} --checkpoints-dir "./saved_models" --optimizer ${optimizer} --dataset ${dataset} --identity-loss ${identity_loss} --dataset_name ${dataset_name}  --num ${N} --extra-dim ${extra_dim} --batch-size ${batch_size} --epochs ${epochs} --data-dir ./data --save-interval ${save_interval} --plot 0 --equiv-loss ${equiv_loss}
 python3 visualize_latent_space.py --save-folder=${model_name} --dataset=rot-arrows --dataset_name ${dataset_name}
