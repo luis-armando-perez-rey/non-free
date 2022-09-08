b50614c1d6d6d9ca0344af5b60921f6d09740bde
luis-armando-perez-rey <57133495+luis-armando-perez-rey@users.noreply.github.com>
2022-09-08 10:23:40 +0200

    Update run.sh and generate.sh
    Update run.sh and generate.sh with calls using the new flags in the scritps

--- generate_data.sh
+++ generate_data.sh
@@ -1,9 +1,16 @@
-#!/bin/sh
+#!/bin/bash
+source /home/TUE/s161416/miniconda3/etc/profile.d/conda.sh
+source activate gpytorch
+# Sinusoidal data generation
 python3 generate_data.py --n_examples 10000 --dataset_name 1 --n_arrows 1 --dataset sinusoidal
 python3 generate_data.py --n_examples 10000 --dataset_name 2 --n_arrows 2 --dataset sinusoidal
 python3 generate_data.py --n_examples 10000 --dataset_name 3 --n_arrows 3 --dataset sinusoidal
-python3 generate_data.py --n_examples 10000 --dataset_name 5 --n_arrows 5 --dataset sinusoidal
+python3 generate_data.py --n_examples 10000 --dataset_name 3 --n_arrows 5 --dataset sinusoidal
 python3 generate_data.py --n_examples 10000 --dataset_name 7 --n_arrows 7 --dataset sinusoidal
-#python3 generate_data.py --n_examples 10000 --dataset_name 6 --n_arrows 1
-#python3 generate_data.py --n_examples 10000 --dataset_name 4_m --n_arrows 4 --multicolor
+# Arrows data generation
+python3 generate_data.py --n_examples 10000 --dataset_name 1 --n_arrows 1 --dataset rot-arrows
+python3 generate_data.py --n_examples 10000 --dataset_name 2 --n_arrows 2 --dataset rot-arrows
+python3 generate_data.py --n_examples 10000 --dataset_name 3 --n_arrows 3 --dataset rot-arrows
+python3 generate_data.py --n_examples 10000 --dataset_name 5 --n_arrows 5 --dataset rot-arrows
+python3 generate_data.py --n_examples 10000 --dataset_name 7 --n_arrows 7 --dataset rot-arrows
 
